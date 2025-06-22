7de78781e808428cd175b1c58330c03df69a53be
Aditi Kansara <aditi.kansara@mercedes-benz.com>
2025-06-22 18:17:10 +0530

    d7 d8 added

--- d7_Hangman.py
+++ d7_Hangman.py
@@ -1,71 +1,9 @@
 #Hangman
-hanglist = [
-    '''
-    _______
-    |/     /|
-    |      (_)
-    |      \\|/
-    |       |
-    |      / \\
-    |
- ___|___
-''',
-    '''
-    _______
-    |/     /|
-    |      (_)
-    |      \\|/
-    |       |
-    |      /
-    |
- ___|___
-''',
-    '''
-    _______
-    |/      |
-    |      (_)
-    |      \\|/
-    |       |
-    |      
-    |
- ___|___
-''',
-    '''
-    _______
-    |/      |
-    |      (_)
-    |      \\|/
-    |       
-    |      
-    |
- ___|___
-''',
-    '''
-    _______
-    |/      |
-    |      (_)
-    |      
-    |       
-    |      
-    |
- ___|___
-''',
-    '''
-    _______
-    |/      |
-    | 
-    |      
-    |       
-    |      
-    |
- ___|___
-'''
-]
-
-word_list = ["panipuri", "fafda", "jalebi", "vadapav"]
+import d7_HangmanWords
 import random
 
-current_word = random.choice(word_list)
+print(d7_HangmanWords.hangman_logo)
+current_word = random.choice(d7_HangmanWords.word_list)
 
 final_word = "_"*len(current_word)
 print(final_word)
@@ -75,6 +13,13 @@ lives = 5
 while not gameOver:
     display_word = ""
     guessed_letter = input("Guess a letter: ").lower()
+
+    if guessed_letter not in current_word:
+        print(f"You guessed {guessed_letter}, that's not in word. You lose life!")
+        lives = lives - 1
+    else:
+        print(f"You have already guessed {guessed_letter}, guess a new word!")
+
     for letter in current_word:
         if letter == guessed_letter:
             display_word += letter
@@ -83,17 +28,15 @@ while not gameOver:
             display_word+= letter
         else:
             display_word+= "_"
-        
-    if guessed_letter not in current_word:
-        lives = lives - 1
 
     print(display_word)
-    print(hanglist[lives])
-
+    print(d7_HangmanWords.hanglist[lives])
+    print(f"******************* {lives} LIVES LEFT********************")
+    
     if "_" not in display_word:
-        print("You win!")
+        print("************************YOU WIN*************************")
         gameOver = True
     
     if lives == 0:
-        print("you lose!")
+        print(f"******************YOU LOSE, IT WAS {current_word}*******************")
         gameOver = True
---
+++ d8_CeaserCipher.py
@@ -0,0 +1,25 @@
+#CaeserCipher
+import logo
+print(logo.caesercipher_logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+ 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+rerun = "yes"
+
+def caeser(msg,key,choose):
+    output = ""
+    if choose == "decode":
+           key *= -1
+    for letter in msg:
+        if letter in alphabet:
+            output += alphabet[(alphabet.index(letter)+key)%len(alphabet)]
+        else:
+            output += letter
+    print(f"Here's the output version {output}")
+    
+while rerun == "yes":
+    choose= input("Type 'encode' to encrypt and 'decode' to decrypt:\n").lower()
+    msg = input("Type your message:\n").lower()
+    key = int(input("Type your key:\n"))
+    caeser(msg,key,choose)
+    rerun = input("Type yes if you want to run again, otherwise no\n").lower()
