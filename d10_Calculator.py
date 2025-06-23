c537c4b701fbe61618c6b9185d3c8fafc3a6bdc6
Aditi Kansara <aditi.kansara@mercedes-benz.com>
2025-06-23 15:01:52 +0530

    d9 d10 added

---
+++ d10_Calculator.py
@@ -0,0 +1,42 @@
+#Calculator
+import logo
+
+def sum(num1,num2):
+    return num1+num2
+
+def minus(num1,num2):
+    return num1-num2
+
+def multiply(num1,num2):
+    return num1*num2
+
+def divide(num1,num2):
+    return num1/num2
+
+operatinos = {"+": sum,
+              "-": minus,
+              "*": multiply,
+              "/": divide}
+
+def calc():
+    print(logo.calc_logo)
+    should_continue = "y"
+    num1 = int(input("What's first number?: "))
+
+    while should_continue == "y":
+        print("+\n-\n*\n/\n")
+        op = input("pick operation: ")
+        num2 = int(input("What's next number?: "))
+        
+        ans = operatinos[op](num1,num2)
+        print(f"{num1}{op}{num2}={ans}")
+
+        should_continue = input("Type y to continue calculating, or type n to start new.")
+        if should_continue == "y":
+            num1 = ans
+        else:
+            should_continue == "n"
+            print("\n"*20)
+            calc()
+
+calc()
---
+++ d9_AuctionProgram.py
@@ -0,0 +1,21 @@
+#AuctionProgram
+import logo
+print(logo.auction_logo)
+print("Welcome to the secret auction program.")
+others = "yes"
+bid = {}
+while others == "yes":
+    name = input("What is your name? ")
+    bid_value = int(input("What is your bid? $"))
+    others = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+    print("\n"*100)
+    
+    bid[name] = bid_value
+
+max_bid = 0
+winner = ""
+for bidder in bid:
+    if bid[bidder] > max_bid:
+        max_bid = bid[bidder]
+        winner = bidder
+print(f"The winner is {winner} with a bid of ${max_bid}.")
